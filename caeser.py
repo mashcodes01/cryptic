e03e68522050a640ced844bfcc6800c0dc6719d8
Md. Mashiur Rahman Khan <83354426+mashcodes10@users.noreply.github.com>
2022-11-15 13:22:39 +0600

    Update caeser.py

--- caeser.py
+++ caeser.py
@@ -18,3 +18,19 @@ s = 4
 print "Plain Text : " + text
 print "Shift pattern : " + str(s)
 print "Cipher: " + encrypt(text,s)
+
+message = 'GIEWIVrGMTLIVrHIQS' #encrypted message
+LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+for key in range(len(LETTERS)):
+   translated = ''
+   for symbol in message:
+      if symbol in LETTERS:
+         num = LETTERS.find(symbol)
+         num = num - key
+         if num < 0:
+            num = num + len(LETTERS)
+         translated = translated + LETTERS[num]
+      else:
+         translated = translated + symbol
+print('Hacking key #%s: %s' % (key, translated))
